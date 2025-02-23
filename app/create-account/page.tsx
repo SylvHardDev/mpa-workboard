b97acce9b4b2a1f1730d42d25c9bdd3916ebcae7
HardDev <139948740+SylvHardDev@users.noreply.github.com>
2025-02-24 00:12:28 +0300

    ui auth (#4)
    Co-authored-by: HardyNuklear <hardy.herinavalona@nuklear.io>

---
+++ app/create-account/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { CreateAccountForm } from "./CreateAccountForm";
+
+const CreateAccountPage = () => {
+  return (
+    <div className="flex items-center justify-center h-minus-80">
+      <CreateAccountForm />
+    </div>
+  );
+};
+
+export default CreateAccountPage;
